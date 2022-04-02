22686d62eebd3ec0647b448ea4c1a32e1dab884e
Viraj Patidar <vrj381@gmail.com>
2022-04-02 13:32:37 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+import NotFound from './components/notFound';
+import Home from './components/home';
+
+// MUI
+import { blueGrey } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: blueGrey[900],
+        },
+    },
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Routes>
+                
+                <Route path="/" element={<Home />} exact />
+                <Route path="*" element={<NotFound />} />
+
+            </Routes>
+        </ThemeProvider>
+    );
 }
 
 export default App;
